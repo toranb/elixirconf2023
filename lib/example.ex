fe5457fd8afff9c107d83cfe9009f399f6dacc92
toran billups <toranb@gmail.com>
2023-07-29 10:19:54 -0500

    added simple encoding task to show the vocab token ids

--- lib/example.ex
+++ lib/example.ex
@@ -8,4 +8,12 @@ defmodule Example do
   def evaluate() do
     Example.Evaluate.cancellations()
   end
+
+  def encodings() do
+    input = "the price was outrageous"
+    {:ok, tokenizer} = Tokenizers.Tokenizer.from_pretrained("bert-base-uncased")
+    {:ok, encoding} = Tokenizers.Tokenizer.encode(tokenizer, input)
+    Tokenizers.Encoding.get_tokens(encoding) |> IO.inspect(label: "vocab token ids")
+    Tokenizers.Encoding.get_ids(encoding) |> IO.inspect(label: "encoding")
+  end
 end
