6b7e39298f9c30f098659c51f36b4a152f16871c
toran billups <toranb@gmail.com>
2023-08-19 07:09:05 -0500

    added function to get token ids and embeddings

--- lib/example.ex
+++ lib/example.ex
@@ -13,11 +13,27 @@ defmodule Example do
     Example.Evaluate.cancellations()
   end
 
-  def encodings() do
-    input = "the price was outrageous"
+  def get_token_ids() do
+    # [101, 1996, 3976, 2001, 25506, 102]
+    text = "the price was outrageous"
     {:ok, tokenizer} = Tokenizers.Tokenizer.from_pretrained("bert-base-uncased")
-    {:ok, encoding} = Tokenizers.Tokenizer.encode(tokenizer, input)
-    Tokenizers.Encoding.get_tokens(encoding) |> IO.inspect(label: "vocab token ids")
-    Tokenizers.Encoding.get_ids(encoding) |> IO.inspect(label: "encoding")
+    {:ok, encoding} = Tokenizers.Tokenizer.encode(tokenizer, text)
+    Tokenizers.Encoding.get_tokens(encoding) |> IO.inspect(label: "tokens")
+    Tokenizers.Encoding.get_ids(encoding) |> IO.inspect(label: "token ids")
+  end
+
+  def embeddings(token_ids) when is_list(token_ids) do
+    {:ok, %{model: model, params: params}} =
+      Bumblebee.load_model({:hf, "bert-base-cased"},
+        architecture: :base
+      )
+
+    inputs = %{
+      "input_ids" => Nx.tensor([token_ids])
+    }
+
+    outputs = Axon.predict(model, params, inputs)
+
+    outputs.hidden_state
   end
 end
