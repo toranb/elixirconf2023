2e61d89570ef05139cd24d0ec1226593d7e0663a
toran billups <toranb@gmail.com>
2023-08-26 08:28:46 -0500

    simplify the cancellations dataset

--- lib/example/evaluate.ex
+++ lib/example/evaluate.ex
@@ -33,33 +33,10 @@ defmodule Example.Evaluate do
     "priv/data/july.csv"
     |> File.stream!()
     |> OrdersParser.parse_stream()
-    |> Enum.map(fn [
-                     id,
-                     total,
-                     text,
-                     delta,
-                     close_reason,
-                     previous,
-                     month,
-                     inserted_at,
-                     rfq_state,
-                     state
-                   ] ->
+    |> Enum.map(fn [id, text] ->
       %{result: [reason]} = Nx.Serving.batched_run(ExampleClassification, text)
 
-      %{
-        id: id,
-        total: total,
-        reason: reason,
-        delta: delta,
-        close_reason: close_reason,
-        previous: previous,
-        month: month,
-        inserted_at: inserted_at,
-        rfq_state: rfq_state,
-        state: state,
-        original: text
-      }
+      %{id: id, reason: reason, text: text}
     end)
     |> writecsv()
   end
@@ -70,21 +47,7 @@ defmodule Example.Evaluate do
 
     data
     |> List.flatten()
-    |> Enum.map(
-      &[
-        &1.reason,
-        &1.id,
-        &1.total,
-        &1.delta,
-        &1.close_reason,
-        &1.previous,
-        &1.month,
-        &1.inserted_at,
-        &1.rfq_state,
-        &1.state,
-        &1.original
-      ]
-    )
+    |> Enum.map(&[&1.reason, &1.text])
     |> CSV.encode()
     |> Enum.each(&IO.write(file, &1))
   end
